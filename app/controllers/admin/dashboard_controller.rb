4cd518b40e6082f322096e9507c5ddbb7ce021c9
Robert Shum <robertshum@gmail.com>
2023-11-15 12:16:45 -0800

    add dashboard for admin page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,10 @@ class Admin::DashboardController < ApplicationController
   include AuthenticationConcern
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
+    @avg_order_amount = Order.average("total_cents")
+    @max_order_amount = Order.maximum("total_cents")
+    @min_order_amount = Order.minimum("total_cents")
   end
 end
