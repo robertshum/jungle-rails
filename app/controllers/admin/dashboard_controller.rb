ed55bddf148e83cd601dd5141eef1335cdf0e575
Robert Shum <robertshum@gmail.com>
2023-11-15 16:44:24 -0800

    add category f(n) and routes.  Fix decimal bug on dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,8 +6,10 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
-    @avg_order_amount = Order.average("total_cents")
-    @max_order_amount = Order.maximum("total_cents")
-    @min_order_amount = Order.minimum("total_cents")
+
+    # starting out, we have no orders, so default it to 0  If we try to divide 0 by 100 in the form it wil throw an error.
+    @avg_order_amount = Order.average("total_cents") || 0
+    @max_order_amount = Order.maximum("total_cents") || 0
+    @min_order_amount = Order.minimum("total_cents") || 0
   end
 end
